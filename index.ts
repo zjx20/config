8b612e3b1da5ca542fffffcf0faf9d381098540d
Burak Tasci <me@fulls1z3.com>
2016-11-23 17:30:10 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { ConfigLoader, ConfigStaticLoader, ConfigService } from './src/config.service';
+
+export * from './src/config.service';
+
+export function configLoaderFactory() {
+    return new ConfigStaticLoader();
+}
+
+@NgModule()
+export class ConfigModule {
+    static forRoot(providedLoader: any = {
+                       provide: ConfigLoader,
+                       useFactory: (configLoaderFactory)
+                   }): ModuleWithProviders {
+        return {
+            ngModule : ConfigModule,
+            providers : [
+                providedLoader,
+                ConfigService
+            ]
+        };
+    }
+}
